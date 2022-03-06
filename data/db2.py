de2b80fe9f9a7aa88854a2ee38c1905da4963197
jaxineC <jaxine.c@gmail.com>
2022-03-06 12:14:36 +0800

    part 1-1 fix2

--- data/db2.py
+++ data/db2.py
@@ -41,55 +41,14 @@ col_list=[]
 for n in data['result']['results'][0]:
   col_list.append(n)
 
-#----------------------------------------------------------------------2)
-sql= f'''CREATE TABLE attractions (
-  {col_list[0]} VARCHAR(1024) NOT NULL, 
-  {col_list[1]} VARCHAR(255) NOT NULL, 
-  {col_list[2]} DATE NOT NULL, 
-  {col_list[3]} DECIMAL(9,6) NOT NULL DEFAULT "0", 
-  {col_list[4]} VARCHAR(255) NOT NULL, 
-  {col_list[5]} DATE NOT NULL,
-  {col_list[6]} INT NOT NULL, 
-  {col_list[7]} VARCHAR(255), 
-  {col_list[8]} BIGINT NOT NULL, 
-  {col_list[9]} INT NOT NULL, 
-  {col_list[10]} VARCHAR(255) NOT NULL, 
-  {col_list[11]} VARCHAR(255) NOT NULL, 
-  {col_list[12]} VARCHAR(1024), 
-  {col_list[13]} VARCHAR(255) NOT NULL,
-  {col_list[14]} TEXT NOT NULL, 
-  {col_list[15]} VARCHAR(255) NOT NULL, 
-  {col_list[16]} DECIMAL(9,6) NOT NULL DEFAULT "0", 
-  {col_list[17]} TEXT NOT NULL, 
-  {col_list[18]} BIGINT NOT NULL AUTO_INCREMENT, 
-  {col_list[19]} DATE NOT NULL, 
-  {col_list[20]} VARCHAR(255) NOT NULL, 
-  PRIMARY KEY ({col_list[18]}));'''
-cursor_query.execute(sql)
-cnx.commit()
 
-#----------------------------------------------------------------------3)
 for attractions in  range(len(data['result']['results'])):
-  col_list_indivisual=[]
-  for items in data['result']['results'][attractions]:
-    col_list_indivisual.append(items)
-  col_list_individual_str = ', '.join(col_list_indivisual)
-
-  val_list_indivisual=[]
-  for items in data['result']['results'][attractions]:   
-    x = data['result']['results'][attractions][items]
-    val_list_indivisual.append(x)
-  val_list_individual_str = str(val_list_indivisual).replace("[","").replace("]","").replace("None","null")
-
-  sql =f"INSERT INTO attractions ({col_list_individual_str}) VALUES ({val_list_individual_str});"
-  cursor_query.execute(sql)
-  cnx.commit()
-
+ 
   file_ls_0 = data['result']['results'][attractions]['file'].lower().replace("ghttps","g+https").split("+")
   file_ls_1 = [x for x in file_ls_0 if (".jpg" or ".png") in x]
   data['result']['results'][attractions]['file'] = '['+', '.join(file_ls_1)+']'
   
-  sql ="UPDATE attractions SET file= %s WHERE (_id = %s);"
+  sql ="UPDATE attractions SET file= %s WHERE _id = %s;"
   val =(data['result']['results'][attractions]['file'],data['result']['results'][attractions]['_id'])
   cursor_query.execute(sql,val)
   cnx.commit()
